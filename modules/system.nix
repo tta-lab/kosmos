8472eb11c8e18d2dcf119fe4280d360a9884ad7d
Neil <bn0010100@gmail.com>
2026-05-08 16:12:18 +0800

    fix(system): correct duplicated SSH block and add missing module preamble

--- modules/system.nix
+++ modules/system.nix
@@ -1,12 +1,9 @@
+{ config, pkgs, ... }:
 
+{
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
-  services.openssh = {
-    enable = true;
-    settings = {
-      PasswordAuthentication = false;
-      PermitRootLogin = "prohibit-password";
-    };
-  
   services.openssh = {
     enable = true;
     settings = {
@@ -15,5 +12,15 @@
     };
   };
 
-};
+  # Nix settings
+  nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
+    auto-optimise-store = true;
+  };
 
+  # Timezone/locale defaults
+  time.timeZone = "UTC";
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  system.stateVersion = "25.05";
+}
