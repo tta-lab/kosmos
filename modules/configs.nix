a2ebf46b56a84feae9082aab9f20b1da98b5eb3d
Neil <bn0010100@gmail.com>
2026-05-11 20:49:32 +0800

    feat(shell): enable starship prompt

--- modules/configs.nix
+++ modules/configs.nix
@@ -70,6 +70,23 @@ in
           };
         };
 
+        starship = {
+          enable = true;
+          enableFishIntegration = true;
+          settings = {
+            add_newline = false;
+            format = "$directory$git_branch$git_status$character";
+            character = {
+              success_symbol = "[>](bold green)";
+              error_symbol = "[>](bold red)";
+            };
+            directory = {
+              truncation_length = 3;
+              truncate_to_repo = false;
+            };
+          };
+        };
+
         git = {
           enable = true;
           userName = "neil";
