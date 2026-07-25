09b0000857f836455ed4db4654e0a90f0686a335
neil <bn0010100@gmail.com>
2026-07-26 00:15:44 +0800

    feat(kepos): allow Guion node subscribers

--- modules/wsl/kepos-neo.nix
+++ modules/wsl/kepos-neo.nix
@@ -17,6 +17,10 @@
         "c5a2168e17a53b699ced7e3f3c8470afd7f91b97a1582076c9797c3e024311a2"
         "80745ccfb5cb1ec8f10faf19225c0add320b1dc2e3a65914f3789935422fee96"
         "e7cd23d4729148b6a6682c65787be743d63c48f96a3a2cb76ff07a72547be77e"
+        # guion-worker-1
+        "ff9e2bee88a324ccf9ccdcc680a597e8798d008d57b54a4ae2873d26ddfea43e"
+        # guion-worker-2
+        "682276873f44fd590054f68af34798651089b34d5dc70d9ecd151e8bd1a03a90"
       ];
       services = {
         forgejo = {
