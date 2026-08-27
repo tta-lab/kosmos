298d36b04e6172e3cec45c267872930543bb4003
neil <bn0010100@gmail.com>
2026-08-27 13:11:13 +0800

    fix(ssh): replace android key

--- modules/users/neil.nix
+++ modules/users/neil.nix
@@ -8,7 +8,7 @@ _: {
     initialPassword = "changeme";
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOI1HtKeFR8rm4DrSx7pyF5J/gYWmRzccwM7wOAi2yB1 neil@neilmac"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG1lFp9v39RA16BKELDv0n0FNVumRkNz3fhZ1TwXPEHH redock-android"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHnfoOocpi8DgLeI5D+Jlr+yTQyHeEw6WA9eFKdCxUnC"
     ];
   };
 }
